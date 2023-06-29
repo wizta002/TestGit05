f8e1d8af1f345c2e9cbed9a6f973debae78cfe4c
Win Pattarapanee <win091044@gmail.com>
2023-06-29 16:01:13 +0700

    NODEjs CLASS

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+    
+
+        <h2>Home</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis maiores excepturi iste consectetur ratione expedita sed soluta culpa aliquid saepe voluptates, molestiae dolorem enim sunt porro non quibusdam id sint!</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis maiores excepturi iste consectetur ratione expedita sed soluta culpa aliquid saepe voluptates, molestiae dolorem enim sunt porro non quibusdam id sint!</p>
+        
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    h2 {
+        margin-bottom:  20px;
+        font-size: 36px;
+
+    }
+
+    p {
+        margin: 20px 0;
+    }
+
+</style>
---
+++ pages/products/[id].vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <p>Product details for {{ id }}</p>
+    </div>
+</template>
+
+<script setup>
+ const { id } = useRoute().params
+ definePageMeta({
+        layout:'products'
+    })
+
+
+
+</script>
+
+<style scoped>
+
+</style>
