dfd60524e5fdfe739526825ffce5230d32541787
Win Pattarapanee <win091044@gmail.com>
2023-07-04 16:21:34 +0700

    Nodejs-css

---
+++ my-nuxt/pages/about.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h2>About</h2>
+        <p>The Harrods motto is Omnia Omnibus Ubique, which is Latin for "all things for all people, everywhere". Several of its departments, including the Seasonal Christmas department and the Food Halls, are well known.</p>
+        <p>Harrods was also a founder of the International Association of Department Stores in 1928, which is still active today, and remained a member until 1935.Franck Chitham, Harrods' president at the time, was president of the Association in 1930.</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    h2 {
+        margin-bottom:  20px;
+        font-size: 36px;
+    }
+    p {
+        margin: 20px 0;
+    }
+
+</style>
---
+++ my-nuxt/pages/products/[id].vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <p>Product details for {{ id }}</p>
+    </div>
+</template>
+
+<script setup>
+    const { id } = useRoute().params
+
+    definePageMeta({
+        layout:'products'
+    })
+    
+</script>
+
+<style scoped>
+
+</style>
