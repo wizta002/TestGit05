edef9ede74bb1b0d27aaeb8412f3238b3b699aac
Win Pattarapanee <win091044@gmail.com>
2023-07-18 16:04:22 +0700

    my-nuxt3-project

---
+++ my-nuxt3-project/pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h2>Home</h2>
+        <p>Harrods Limited is a department store located on Brompton Road in Knightsbridge, London, England. It is currently owned by the state of Qatar via its sovereign wealth fund, the Qatar Investment Authority. The Harrods brand also applies to other enterprises undertaken by the Harrods group of companies, including Harrods Estates, Harrods Aviation and Air Harrods.</p>
+        <p>The store occupies a 5-acre (2 ha) site and has 330 departments covering 1.1 million sq ft (100,000 m2) of retail space. It is one of the largest and most famous department stores in the world.</p>
+    
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    h2 {
+        margin-bottom:  20px;
+        font-size: 36px;
+    }
+    p {
+        margin: 20px 0;
+    }
+
+</style>
