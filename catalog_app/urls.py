83d917f3314564e7513685ea74ff72afbcc4f732
Win Pattarapanee <win091044@gmail.com>
2023-09-30 21:32:00 +0700

    Assignment2

---
+++ catalog_app/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+    path('', views.homepage, name='homepage'),
+    path('product_list', views.product_list, name='product list'),
+    path('product_detail', views.product_detail, name='product detail'),
+    path('register', views.register, name='register'),
+    path('register2', views.register2, name='register2'),
+    path('login', views.login, name='login'),
+    path('login2', views.login2, name='login2'),
+    path('app_login/', views.app_login, name='app_login'),
+]
